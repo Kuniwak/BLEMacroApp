05c24413b126201e04abc1fa1b1f4e9638ed0c84
Kuniwak <yuki.kokubun@dena.com>
2024-06-10 07:23:06 +0900

    Initialize project

---
+++ BLEMacroApp/BLEMacroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEMacroAppApp.swift
+//  BLEMacroApp
+//
+//  Created by Yuki Kokubun on 2024/06/10.
+//
+
+import SwiftUI
+
+@main
+struct BLEMacroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEMacroApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLEMacroApp
+//
+//  Created by Yuki Kokubun on 2024/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
